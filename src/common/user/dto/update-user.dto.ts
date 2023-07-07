df9d33bdf6f1e94a5cd37ee4c6871331d54ccc24
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-07-07 11:47:43 -0300

    feat: implemented filter for HttpException

--- src/common/user/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { SignUserDto } from 'src/common/auth/dto/sign-user.dto';
-
-export class UpdateUserDto extends PartialType(SignUserDto) {}
