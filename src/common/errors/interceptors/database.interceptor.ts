23978a43819a1245532c0f42575ad219eb01c9b1
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-07-07 13:58:41 -0300

    feat: implemented database interceptor

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -5,13 +5,15 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable, catchError } from 'rxjs';
+import { isPrismaError } from 'src/common/utils/is-prisma-error.util';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error) => {
-        console.log(error);
+        if (isPrismaError(error)) {
+        }
         throw error;
       }),
     );
