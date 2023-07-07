6cc37dc770c612728bc6cc195896169d62e5fe83
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-07-07 13:41:40 -0300

    feat: implemented error handling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from './user.repository';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  findAll() {
-    return this.userRepository.findAll();
+  async findAll() {
+    const users = await this.userRepository.findAll();
+    if (users.length === 0) throw new NotFoundError();
+    return users;
   }
 
-  findOne(id: string) {
-    return this.userRepository.findOne(id);
+  async findOne(id: string) {
+    const user = await this.userRepository.findOne(id);
+    if (!user) throw new NotFoundError();
+    return user;
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update(id, updateUserDto);
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.userRepository.update(id, updateUserDto);
+    if (!user) throw new NotFoundError();
+    return user;
   }
 
-  remove(id: string) {
-    return this.userRepository.remove(id);
+  async remove(id: string) {
+    const user = await this.userRepository.remove(id);
+    if (!user) throw new NotFoundError();
+    return user;
   }
 }
